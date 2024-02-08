9d3e20ad0cf3fc65a9599b7e73a8701444154e96
Jasmijn-Ginger <jasmijn.h@live.nl>
2024-02-08 13:55:07 +0100

    Fixed commentary

--- PasswordGenerator/src/main/InputUser.java
+++ PasswordGenerator/src/main/InputUser.java
@@ -4,11 +4,13 @@ public class InputUser {
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
+    //asks in terminal amount of characters
     public static int askAmountOfCharacters() {
         System.out.println("How many characters do you want your password to contain? It must be at least 4.");
         return SCANNER.nextInt();
     }
 
+    //asks in terminal if you want another password
     public static String askRepeat(){
         System.out.println("Do you want another password? ");
         return SCANNER.next();
